5f856a3d40fb0a596ecb361e8cc71e814f72120d
harunkara <harunkaracoban00@gmail.com>
2022-10-10 16:36:29 +0300

    eslint added and indentation issues solved

--- src/actions/getRandomUsersAction.js
+++ src/actions/getRandomUsersAction.js
@@ -1,40 +1,40 @@
-import axios from "axios";
+import axios from 'axios';
 
 function request(namespace) {
     return {
-      type: `${namespace}/GET_REQUEST`,
+        type: `${namespace}/GET_REQUEST`,
     };
-  }
+}
   
-  function success(namespace, data) {
+function success(namespace, data) {
     return {
-      type: `${namespace}/GET_SUCCESS`,
-      data,
+        type: `${namespace}/GET_SUCCESS`,
+        data,
     };
-  }
+}
   
-  function failure(namespace, error) {
+function failure(namespace, error) {
     return {
-      type: `${namespace}/GET_FAILURE`,
-      error,
+        type: `${namespace}/GET_FAILURE`,
+        error,
     };
-  }
+}
 export function getRandomUsersAction(namespace){
     return dispatch => {
         dispatch(request(namespace));
         axios.get(namespace)
-          .then(res => {
-            if (res.data.errorCode) {
-             console.log('Error:',res.data.errorCode);
-            } else {
-              dispatch(success(namespace, res.data));
-            }
-          })
-          .then(() => {
-          })
-          .catch((error) => {
-            dispatch(failure(namespace, error));
-            console.log('Error:',error);
-          });
-      };
+            .then(res => {
+                if (res.data.errorCode) {
+                    console.log('Error:',res.data.errorCode);
+                } else {
+                    dispatch(success(namespace, res.data));
+                }
+            })
+            .then(() => {
+            })
+            .catch((error) => {
+                dispatch(failure(namespace, error));
+                console.log('Error:',error);
+            });
+    };
 }
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,23 +1,35 @@
-import React from 'react'
-import '../css/usercard.css'
+/* eslint-disable react/prop-types */
+import React from 'react';
+import '../css/usercard.css';
 import PersonIcon from '@mui/icons-material/Person';
 import MailIcon from '@mui/icons-material/Mail';
 import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 const UserCard=(props)=> {
-  const {user}=props;
+    const {user}=props;
 
-  return (
-<div className='user__card__container'>
-<div className='user__card'>
-      {console.log("girdim")}
-      <img className='user__card__picture' src={user?.picture?.large}/>
-      <div className='user__card__row'><PersonIcon className="mui__icons"></PersonIcon><span>{user?.name?.first} {user?.name?.last}</span></div>
-      <div className='user__card__row'><MailIcon className="mui__icons"></MailIcon><span>{user?.email}</span></div>
-      <div className='user__card__row'><PhoneIphoneIcon className="mui__icons"></PhoneIphoneIcon><span>{user?.cell}</span></div>
-      <div className='user__card__row'><LocationOnIcon className="mui__icons"></LocationOnIcon><span>{user?.location?.city}</span></div>
-    </div>
-</div>
-  )
-}
-export default UserCard
+    return (
+        <div className='user__card__container'>
+            <div className='user__card'>
+                <img className='user__card__picture' src={user?.picture?.large}/>
+                <div className='user__card__row'>
+                    <PersonIcon className="mui__icons"></PersonIcon>
+                    <span>{user?.name?.first} {user?.name?.last}</span>
+                </div>
+                <div className='user__card__row'>
+                    <MailIcon className="mui__icons"></MailIcon>
+                    <span>{user?.email}</span>
+                </div>
+                <div className='user__card__row'>
+                    <PhoneIphoneIcon className="mui__icons"></PhoneIphoneIcon>
+                    <span>{user?.cell}</span>
+                </div>
+                <div className='user__card__row'>
+                    <LocationOnIcon className="mui__icons"></LocationOnIcon>
+                    <span>{user?.location?.city}</span>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default UserCard;
